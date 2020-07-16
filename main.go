99a0d2aa31e0609c66fe1d88f01c1824c32984fb
Karthik <40568655+karthiksedoc@users.noreply.github.com>
2020-07-16 23:16:09 +0530

    created array of domains to search against every request URI

--- main.go
+++ main.go
@@ -13,10 +13,26 @@ import (
 //	return http.HandlerFunc(cust_handler)
 //}
 
+func domainsearch(b string) string {
+	domains := [3] string{"www.google.com", "facebook.com", "yahoo.com"}
+	fmt.Println(b)
+	for _, name := range domains  {
+		fmt.Println(name)
+		if name == b {
+			return("Denied")
+		} else {
+			return("Ok")
+		}
+	} 
+    return("Ok")
+}
+
 func proxyserver(w http.ResponseWriter, r *http.Request) {
 	domain_name := r.Host
 //	w.Write([]byte(domain_name))
-	fmt.Fprintf(w, "%v\n", domain_name)
+	status := domainsearch(domain_name)
+
+	fmt.Fprintf(w, "%v\n", status)
 }
 
 func main() {
