a4090b6478283143a0646e68bdbfb47519409182
Karthik <40568655+karthiksedoc@users.noreply.github.com>
2020-07-26 16:46:46 +0530

    included http client handle

--- main.go
+++ main.go
@@ -3,34 +3,50 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
 
-//func proxyserver(domain string) http.Handler{
-//	cust_handler := func(w http.ResponseWriter, r *http.Request) {
-//		domain_name := r.URL
-//		w.Write([]byte*(domain_name))
-//	}
-//	return http.HandlerFunc(cust_handler)
-//}
 
-func domainsearch(b string) string {
-	domains := [3] string{"www.google.com", "facebook.com", "yahoo.com"}
-	fmt.Println(b)
+func httpclient(b string) ([]byte, error) {
+	response, err := http.Get(b)	
+	if err != nil {
+		//Need to handle error
+		var my_response []byte
+		return my_response, err
+	}
+	defer response.Body.Close()
+	my_response, _ := ioutil.ReadAll(response.Body)
+	return my_response, nil
+}
+
+func domainsearch(b string) ([]byte, string) {
+	domains := [4] string{"www.google.com", "google.com", "facebook.com", "yahoo.com"}
+//	fmt.Println(b)
+	var web_response []byte
 	for _, name := range domains  {
 		fmt.Println(name)
 		if name == b {
-			return("Denied")
-		} else {
-			return("Ok")
-		}
-	} 
-    return("Ok")
+	//	   var web_response []byte
+		   dom_status := "Denied"
+		   return web_response, dom_status
+		} 
+	}
+	webresponse, err := httpclient(b)
+	if err != nil {
+		return web_response, "Access Error"
+	}
+    return webresponse, ""
 }
 
 func proxyserver(w http.ResponseWriter, r *http.Request) {
 	domain_name := r.Host
 //	w.Write([]byte(domain_name))
-	status := domainsearch(domain_name)
+	status, err := domainsearch(domain_name)
+
+	if (err == "Denied") {
+	   http.Error(w, "Blocked", 403)
+	   return
+	}
 
 	fmt.Fprintf(w, "%v\n", status)
 }
